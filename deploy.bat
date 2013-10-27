15a44eba28d8f1972ca17dcd10f5d4564bbbf557
Samuel <elsamuko@gmail.com>
2013-10-27 21:46:50 +0100

    no more dlls for distribution

--- deploy.bat
+++ deploy.bat
@@ -13,19 +13,9 @@ XCOPY %CD%\src           %CD%\pathpath\src /E /Y
 
 
 :: copy binary and sample scripts
-CP %CD%\bin\pathpath.exe %CD%\pathpath\pathpath.exe
-CP %CD%\bin\read.bat     %CD%\pathpath\read.bat
-CP %CD%\bin\write.bat    %CD%\pathpath\write.bat
-
-
-:: copy redistributables
-IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-	CP %SYSTEMROOT%\SysWOW64\msvcp100.dll %CD%\pathpath\msvcp100.dll
-	CP %SYSTEMROOT%\SysWOW64\msvcr100.dll %CD%\pathpath\msvcr100.dll
-) ELSE (
-	CP %SYSTEMROOT%\System32\msvcp100.dll %CD%\pathpath\msvcp100.dll
-	CP %SYSTEMROOT%\System32\msvcr100.dll %CD%\pathpath\msvcr100.dll
-)
+COPY %CD%\bin\pathpath.exe %CD%\pathpath\pathpath.exe
+COPY %CD%\bin\read.bat     %CD%\pathpath\read.bat
+COPY %CD%\bin\write.bat    %CD%\pathpath\write.bat
 
 
 :: zip package
